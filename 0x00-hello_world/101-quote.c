ce00f13b7a5fa8853fc696c23e71d13cd11998e6
root <root@DESKTOP-KFBIMQJ>
2023-07-13 14:54:22 +0100

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,8 @@
  */
 int main(void)
 {
-	("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 
 }

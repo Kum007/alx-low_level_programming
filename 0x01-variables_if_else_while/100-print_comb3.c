3dbf3fcb2e2527df890c063ef341130e3705ec1a
Animasaun Malik Kunmi <animamalikunmi123@gmail.com>
2023-07-15 00:33:04 +0100

    10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,16 +8,15 @@ int main(void)
 	int a;
 	int b;
 
-	for (a = 0; a < 10; a++)
+	for (a = 0; a < 9; a++)
 	{
-	for (b = 0; b < 10; b++)
+	for (b = a + 1; b < 10; b++)
 	{
-	putchar(a + '0');
-	putchar(b + '0');
-	if (a == 9 && b == 9)
-	{
-	break;
-	}
+	putchar((a % 10) + '0');
+	putchar((b % 10) + '0');
+	if (a == 8 && b == 9)
+	continue;
+
 	putchar(',');
 	putchar(' ');
 	}

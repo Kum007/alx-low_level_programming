3b8bbe92afbf9a9119539b44d05c21d6e9733d65
root <root@DESKTOP-KFBIMQJ>
2023-07-14 17:37:22 +0100

    files

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - This is the main
+*Return: 0 (Success)
+*/
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+	putchar(a);
+	}
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - This is the main
+ * Return: 0
+ */
+int main(void)
+{
+	char a = 'a';
+
+	for (a != 'q', 'e'; a <= 'z'; a++)
+	{
+	putchar(a);
+	}
+	putchar('\n');
+
+	return (0);
+}

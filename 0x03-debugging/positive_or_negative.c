fa88be846dc2c7d40ff842092c065f2a4fe14ccd
Animasaun Malik Kunmi <animamalikunmi123@gmail.com>
2023-07-20 12:12:27 +0100

    fixbugs

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main -Entry point
+ * Description - program checks if number i is positive or negetive
+ * Return: 0
+ */
+void positive_or_negative(int i)
+/**
+ * positive or negative
+ */
+{
+	if (i > 0)
+	{
+	printf("%d is positive\n", i);
+	}
+	else if (i < 0)
+	{
+	printf("%d is negative\n", i);
+	}
+	else
+	printf("%d is zero\n", i);
+
+}

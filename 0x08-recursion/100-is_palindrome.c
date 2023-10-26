cc32f4dcf2f903d9991f3ac642baa132a03e4ce9
Animasaun Malik <animamalikunmi123@gmail.com>
2023-10-26 15:36:20 +0000

    files

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,1 +1,57 @@
 #include "main.h"
+int if_palindrome(char *s, int a, int len);
+int _strlen(char *s);
+
+int is_palindrome(char *s)
+{
+if(*s == 0)
+	{
+	return(0);
+	}
+else
+	{
+	return(0 + if_palindrome(s, 0, _strlen(s)));
+	}
+}
+
+int if_palindrome(char *s, int a, int len)
+{
+if(*(s + len - 1) != *(s + a))
+	{
+	return(0);
+	}
+if(a >= len)
+	{
+	return(1);
+	}
+else
+	{
+	return(if_palindrome(s, a + 1 ,len - 1));
+	}
+}
+int _strlen(char *s)
+{
+if (*s == '\0')
+	{
+	return(0);
+	}
+else
+	{
+	return(1 + _strlen(s + 1));
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

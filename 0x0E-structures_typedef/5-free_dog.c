36f5edbc2efc2910d669a5eb6e0fcf96029ea702
Animasaun Malik <animamalikunmi123@gmail.com>
2023-11-06 12:41:04 +0000

    files

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+void free_dog(dog_t *d)
+{
+	if(d)
+	{
+		free((*d).name);
+		free((*d).age);
+		free((*d).owner);
+		free(d);
+
+	}
+
+
+}
---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This is the Struct and Typedef README file
